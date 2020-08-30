9f037392eae4e1626e9dd9a1160c65aabaf10a21
Timur <54122011+tkachikaev@users.noreply.github.com>
2020-08-31 01:01:18 +0300

    Work_6_v2.0

--- Modul6/HomeWork6/Program.cs
+++ Modul6/HomeWork6/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HomeWork6
@@ -28,13 +29,74 @@ namespace HomeWork6
             return num;
         }
 
+         
+
+        /// <summary>
+        /// Возвращает кол-во групп для цифр путем (2(group) * 2)num, + группа для 1-цы.
+        /// </summary>
+        /// <param name="num">Пользовательский ввод</param>
+        /// <returns></returns>
+        static int GroupDell(int num)
+        {
+            int group = 1;
+            int count = 2;
+
+            while (true)
+            {
+                count = count * 2;
+                group += 1;
+                if (count >= num)
+                {
+                    return group;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создает массив листов, количество массивов зависит от принимающего значения.
+        /// </summary>
+        /// <param name="group"></param>
+        /// <returns></returns>
+        static List<List<int>> ListFill(int group)
+        {
+            List<List<int>> arrayGroup = new List<List<int>>();
+
+            for (int i = 0; i < group; i++)
+            {
+                arrayGroup.Add(new List<int>());
+            }
+
+            return arrayGroup;
+        }
+
+        /// <summary>
+        /// Возвращает массив с разбиение неделимых чисел в разных массивах.
+        /// </summary>
+        /// <param name="ArrayList">Массив с группами</param>
+        /// <param name="Num">Максимальное число для разбиения</param>
+        /// <returns></returns>
+        //static List<List<int>> ListSplitting(List<List<int>> ArrayList, int Num)
+        //{
+            
+        //}
 
         static void Main(string[] args)
         {
             Console.WriteLine("С каким числом необходмо произвести разделение?");
             int number = ChekNum();
 
+            int group = GroupDell(number);
+            List<List<int>> arrayGroup = ListFill(group);
 
+            #region Надо доделать список пробегания по массиву листов в заполнении списков
+            for (int i = 0; i < arrayGroup.Count; i++)
+            {
+                for (int j = 0; j < arrayGroup[i].Count; j++)
+                {
+
+                }
+            }
+            #endregion
             Console.ReadKey();
         }
     }
